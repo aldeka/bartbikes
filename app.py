becc77fac512f3b68cf106eb6f69f9c1f2fab6d1
Karen Rustad <karen.rustad@gmail.com>
2013-02-02 14:10:27 -0800

    blah

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def populate_chart(data, current_sf_time):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=port)
